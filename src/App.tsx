3fdc59f83b302d5bb95f2071675f16c8543448c6
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-12-16 00:22:59 -0800

    added calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ListsComponent } from "@/components/List";
 import { CategoriesComponent } from "@/components/Category";
+import { Calendar } from "@/components/Calendar";
 
 function App() {
   return (
     <>
+      <Calendar />
       <ListsComponent />
       <CategoriesComponent />
     </>
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+import classes from "@/styles/Calendar.module.css";
+
+const months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+const days = ["☀️", "🌙", "🔥", "💧", "🌳", "✨", "⛰️"];
+
+export function Calendar() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={classes.calendar}>
+      <div>{months[date.getMonth()]}</div>
+      <span />
+      <div>{date.getDate()}</div>
+      <span />
+      <div>{days[date.getDay()]}</div>
+    </div>
+  );
+}
