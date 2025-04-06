db5a513e51cc226eef0c8b19a93159c6169d4f9c
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2025-04-05 23:30:01 -0700

    expanded dateview

--- src/components/DateView.tsx
+++ src/components/DateView.tsx
@@ -31,11 +31,25 @@ export function DateView() {
 
   return (
     <div className={classes.date}>
-      <div>{months[date.getMonth()]}</div>
-      <span />
-      <div>{date.getDate()}</div>
-      <span />
-      <div>{days[date.getDay()]}</div>
+      <div className={classes.header}>
+        <div>Y{date.getFullYear()}</div>
+        <span className={classes.divider} />
+        <div>
+          W
+          {Math.ceil(
+            (date.getTime() - new Date(date.getFullYear(), 0, 1).getTime()) /
+              (1000 * 60 * 60 * 24 * 7),
+          )}
+        </div>
+      </div>
+      <span className={classes.dividerHorizontal} />
+      <div className={classes.body}>
+        <div>{months[date.getMonth()]}</div>
+        <span className={classes.divider} />
+        <div>{date.getDate()}</div>
+        <span className={classes.divider} />
+        <div className={classes.emoji}>{days[date.getDay()]}</div>
+      </div>
     </div>
   );
 }
