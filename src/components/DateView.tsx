c3e95130f2afe812f8a34bb2ab83a792f3b4d07d
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2025-04-16 00:04:29 -0700

    fixed weeks

--- src/components/DateView.tsx
+++ src/components/DateView.tsx
@@ -30,12 +30,24 @@ export function DateView() {
     return () => clearInterval(interval);
   }, []);
 
+  const getWeek = function () {
+    const target = new Date(date.valueOf());
+    const dayNr = (date.getDay() + 6) % 7;
+    target.setDate(target.getDate() - dayNr + 3);
+    const firstThursday = target.valueOf();
+    target.setMonth(0, 1);
+    if (target.getDay() != 4) {
+      target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
+    }
+    return 1 + Math.ceil((firstThursday - target.valueOf()) / 604800000);
+  };
+
   return (
     <div className={classes.date}>
       <div className={classes.header}>
         <div>{date.getFullYear()}</div>
         <Separator />
-        <div>W{String(Math.ceil(date.getDate() / 7)).padStart(2, "0")}</div>
+        <div>W{String(getWeek()).padStart(2, "0")}</div>
       </div>
       <Separator orientation="horizontal" />
       <div className={classes.body}>
