db266f0605448d2d8546d4a182cd5dcd40903428
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2025-04-15 22:19:13 -0700

    fixed dialog

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,18 +1,24 @@
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import * as React from "react";
 
+import classes from "@/styles/Dialog.module.css";
+
 export const Dialog = DialogPrimitive.Root;
 export const DialogTrigger = DialogPrimitive.Trigger;
+export const DialogTitle = DialogPrimitive.Title;
+export const DialogDescription = DialogPrimitive.Description;
 
 export const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ children, ...props }, ref) => (
   <DialogPrimitive.Portal>
-    <DialogPrimitive.Overlay />
-    <DialogPrimitive.Content ref={ref} {...props}>
+    <DialogPrimitive.Overlay className={classes.overlay} />
+    <DialogPrimitive.Content ref={ref} className={classes.content} {...props}>
       {children}
-      <DialogPrimitive.Close aria-label="Close">X</DialogPrimitive.Close>
+      <DialogPrimitive.Close className={classes.closeButton} aria-label="Close">
+        X
+      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPrimitive.Portal>
 ));
